462c241c02abdf4b397c07fbd0e4f16833a15a2f
전연지 <jeonyeonji1028@gmail.com>
2023-10-17 11:18:26 +0900

    peer

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -2,15 +2,19 @@ import type { NextPage } from 'next'
 
 const Forms: NextPage = () => {
   return (
-    <form className="flex flex-col space-y-2 bg-blue-500 p-5 ">
+    <form className="flex flex-col space-y-2 p-5 ">
       <input
         type="text"
         required
         placeholder="Username"
-        className="required:border-2 border-yellow-500 invalid:bg-red-500
-        valid:bg-teal-500"
+        className="border p-1 peer border-gray-400 rounded"
       />
-      <input type="password" required placeholder="Password" />
+      <span className="hidden peer-invalid:block peer-invalid:text-red-500 peer">
+        This input is invalid
+      </span>
+      <span className="hidden peer-valid:block peer-valid:text-teal-500 peer">
+        Awesome username
+      </span>
       <input type="submit" value="Login" className="bg-white" />
     </form>
   )
