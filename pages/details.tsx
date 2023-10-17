f5f950f5aaad9520ee336a7e4b0c90bc588bd2ea
전연지 <jeonyeonji1028@gmail.com>
2023-10-17 14:15:26 +0900

    ul-li, file input, event 중첩

--- pages/details.tsx
+++ pages/details.tsx
@@ -9,6 +9,20 @@ const Forms: NextPage = () => {
           김치
         </span>
       </details>
+
+      <ul className="list-disc marker:text-teal-500">
+        <li>hi</li>
+        <li>hi</li>
+        <li>hi</li>
+        <input
+          type="file"
+          className="file:cursor-pointer 
+          file:hover:text-purple-400 file:hover:bg-white file:hover:border-purple-400 file:hover:border file:transition-colors file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400"
+        />
+        <p className="first-letter:text-2xl first-letter:hover:text-purple-400">
+          test
+        </p>
+      </ul>
     </div>
   )
 }
