f20649d6e39b9462eae37601e5879b24c44fcb53
YeonJi <jeonyeonji1028@gmail.com>
2023-10-19 23:48:00 +0900

    Edit, Sold, Bought, Loved

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -62,9 +62,7 @@ export default function Enter() {
                 <input
                   id="input"
                   type="number"
-                  className="py-2 px-4 border border-transparent appearance-none w-full border-gray-300 
-                  rounded-l-none rounded-r shadow-sm placeholder-gray-400 
-                  focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+                  className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                   required
                 />
               </div>
