396ab8a38577a399f981299bf34e6ddfcd1a6733
전연지 <jeonyeonji1028@gmail.com>
2023-10-31 17:47:02 +0900

    senm sms,  mail

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -1,18 +1,22 @@
 import { useState } from 'react'
 
-export default function useMutation(
+interface UseMutationState<T> {
+  loading: boolean
+  data?: T
+  error?: object
+}
+type UseMutationResult<T> = [(data: any) => void, UseMutationState<T>]
+
+export default function useMutation<T = any>(
   url: string
-): [
-  (data: any) => void,
-  { loading: boolean; data: undefined | any; error: undefined | any }
-] {
-  const [state, setState] = useState({
+): UseMutationResult<T> {
+  const [state, setSate] = useState<UseMutationState<T>>({
     loading: false,
     data: undefined,
     error: undefined,
   })
   function mutation(data: any) {
-    setState((prev) => ({ ...prev, loading: true }))
+    setSate((prev) => ({ ...prev, loading: true }))
     fetch(url, {
       method: 'POST',
       headers: {
@@ -21,10 +25,9 @@ export default function useMutation(
       body: JSON.stringify(data),
     })
       .then((response) => response.json().catch(() => {}))
-      .then((data) => setState((prev) => ({ ...prev, data: data })))
-      .catch((error) => setState((prev) => ({ ...prev, error: error })))
-      .finally(() => setState((prev) => ({ ...prev, loading: false })))
+      .then((data) => setSate((prev) => ({ ...prev, data })))
+      .catch((error) => setSate((prev) => ({ ...prev, error })))
+      .finally(() => setSate((prev) => ({ ...prev, loading: false })))
   }
-
   return [mutation, { ...state }]
 }
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,76 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import client from '@libs/server/client'
-import withHandler from '@libs/server/withHandler'
+import withHandler, { ResponseType } from '@libs/server/withHandler'
+import twilio from 'twilio'
+import smtpTransport from '@libs/server/mail'
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body)
-  return res.status(200).end()
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { phone, email } = req.body
+  const user = phone ? { phone: +phone } : email ? { email } : null
+  if (!user) {
+    return res.json({ ok: false })
+  }
+
+  const payload = Math.floor(100000 + Math.random() * 900000) + ''
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: 'Anonymous',
+            ...user,
+          },
+        },
+      },
+    },
+  })
+  console.log(token)
+
+  if (phone) {
+    /*const mesage = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      from: process.env.SEND_PHONE,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${payload}`,
+    })
+    console.log(mesage)
+    */
+  }
+
+  if (email) {
+    /*
+    const mailOptions = {
+      from: process.env.MAIL_ID,
+      to: process.env.MAIL_ID,
+      subject: 'Nomad Carrot Authentication Email',
+      text: `Authentication Code : ${payload}`,
+    }
+    const result = smtpTransport.sendMail(mailOptions, (error, res) => {
+      if (error) {
+        console.log(error)
+        return null
+      } else {
+        console.log(res)
+        return null
+      }
+    })
+    smtpTransport.close()
+    return console.log(result)
+    */
+  }
+
+  return res.json({
+    ok: true,
+  })
 }
 
 export default withHandler('POST', handler)
